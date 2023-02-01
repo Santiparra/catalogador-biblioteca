7887954e70e47b003b20e8e905dc1e162a4db6c0
santiparra <122759810+Santiparra@users.noreply.github.com>
2023-02-01 09:04:34 -0300

    añadido el event listener para el boton de enviar formulario. creada funcion para evaluar el estado del checkbox.

--- js/javascript.js
+++ js/javascript.js
@@ -9,40 +9,34 @@ let miBiblioteca = [ {
     paginas: 200,
     estado: "no leido",
 }
-
-
-
 ];
 
+let libroNuevo;
 
-function Libro(titulo, autor, paginas, estado, ) {
+function Libro (titulo, autor, paginas, estado) {
     this.titulo = titulo
     this.autor = autor
     this.paginas = paginas
-    this.estado = estado
-    this.informacion = function () {
-        return titulo + autor + paginas + estado;
-    }
+    this.estado = estado  
 }
 
-function agregarLibro() {
+function sacarDatos(e) {
+    e.preventDefault();
     let tituloNuevoLibro = document.getElementById("nombreLibro").value;
     let autorNuevoLibro = document.getElementById("autorLibro").value;
     let paginasNuevoLibro = document.getElementById("paginasLibro").value;
     let estadoNuevoLibro = document.getElementById("estadoLibro").value;
-    
-    let nuevoLibro = Object.create(Libro);
-
-    nuevoLibro.titulo = tituloNuevoLibro;
-    nuevoLibro.autor = autorNuevoLibro;
-    nuevoLibro.paginas = paginasNuevoLibro;
-    nuevoLibro.estado = estadoNuevoLibro; 
-    
-    miBiblioteca.push(nuevoLibro);
+    libroNuevo = new Libro(tituloNuevoLibro, autorNuevoLibro, paginasNuevoLibro, estadoNuevoLibro);    
+    miBiblioteca.push(libroNuevo);
+    refrescarDOM();
+    console.log(libroNuevo);
 }
 
-function buscarLibro() {
+function refrescarDOM() {
     const libros = document.querySelector(".books");
+    while (libros.hasChildNodes()) {
+        libros.removeChild(libros.firstChild);
+    }
     miBiblioteca.forEach(elemento => {
         let libroAAgregar = document.createElement("div");
         libroAAgregar.classList.add("cards");
@@ -62,33 +56,25 @@ function buscarLibro() {
         libroAAgregar.appendChild(autorAAgregar);
         libroAAgregar.appendChild(paginasAAgregar);
         libroAAgregar.appendChild(estadoAAgregar);
-        libros.appendChild(libroAAgregar);
+        libros.appendChild(libroAAgregar);   
     }
-    )
+)
 }
-buscarLibro();
 
+let enviarFormulario = document.getElementById("agregar-libros");
+enviarFormulario.addEventListener("submit", (e) => {
+    sacarDatos(e);
+}
+)  
 
+function valorCheckbox(element) {
+    if(element.checked) 
+        element.value="true"; 
+    else
+       element.value="false";
+}
 
+/* clase añadir form-to-add */
+ /*    enviarFormulario.onsubmit = event.preventDefault();
+   refrescarDOM(); */
 
-/* miBiblioteca.pop(); */
-
-
-
-/* Append Elements
-parentNode.appendChild(childNode) appends childNode as the last child of parentNode
-parentNode.insertBefore(newNode, referenceNode) inserts newNode into parentNode before referenceNode
-Remove Elements
-parentNode.removeChild(child) removes child from parentNode on the DOM and returns a reference to child */
-
-/* div.classList.add('new');                                      
-// adds class "new" to your new div
-
-div.classList.remove('new');                                   
-// removes "new" class from div */
-
-/* funcion que toma user input y lo manda al array
-
-const theHobbit = new Book("The Hobbit", "by J.R.R. Tolkien,","295 pages,","not read yet")
-
-console.log(theHobbit.info()); */
