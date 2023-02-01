f793a5ed22ba0b743d36b8240103bbf8e36f5514
santiparra <122759810+Santiparra@users.noreply.github.com>
2023-02-01 11:45:07 -0300

    Creado generador de ids para cada libro agregado

--- js/javascript.js
+++ js/javascript.js
@@ -1,23 +1,13 @@
-let miBiblioteca = [ {
-    titulo: "hobit",
-    autor: "tokieb",
-    paginas: 300,
-    estado: "leido",
-} , {
-    titulo: "coco",
-    autor: "echaue",
-    paginas: 200,
-    estado: "no leido",
-}
+let miBiblioteca = [ 
 ];
-
 let libroNuevo;
 
-function Libro (titulo, autor, paginas, estado) {
+function Libro (titulo, autor, paginas, estado, id) {
     this.titulo = titulo
     this.autor = autor
     this.paginas = paginas
-    this.estado = estado  
+    this.estado = estado
+    this.id = id  
 }
 
 function sacarDatos(e) {
@@ -26,20 +16,24 @@ function sacarDatos(e) {
     let autorNuevoLibro = document.getElementById("autorLibro").value;
     let paginasNuevoLibro = document.getElementById("paginasLibro").value;
     let estadoNuevoLibro = document.getElementById("estadoLibro").value;
-    libroNuevo = new Libro(tituloNuevoLibro, autorNuevoLibro, paginasNuevoLibro, estadoNuevoLibro);    
+    let idLibro = miBiblioteca.length;
+    libroNuevo = new Libro(tituloNuevoLibro, autorNuevoLibro, paginasNuevoLibro, estadoNuevoLibro, idLibro);    
     miBiblioteca.push(libroNuevo);
     refrescarDOM();
+
     console.log(libroNuevo);
 }
 
 function refrescarDOM() {
     const libros = document.querySelector(".books");
-    while (libros.hasChildNodes()) {
+     while (libros.hasChildNodes()) {
         libros.removeChild(libros.firstChild);
-    }
+    } 
     miBiblioteca.forEach(elemento => {
         let libroAAgregar = document.createElement("div");
         libroAAgregar.classList.add("cards");
+        libroAAgregar.setAttribute("id", miBiblioteca.indexOf(elemento))
+        
         let tituloAAgregar = document.createElement("p");
         tituloAAgregar.classList.add("titulo");
         let autorAAgregar = document.createElement("p");
@@ -48,17 +42,25 @@ function refrescarDOM() {
         paginasAAgregar.classList.add("paginas");
         let estadoAAgregar = document.createElement("p");
         estadoAAgregar.classList.add("estado");
+        let botonBorrar = document.createElement("button");
+        botonBorrar.classList.add("botonBorrado");
         tituloAAgregar.textContent = elemento.titulo;
         autorAAgregar.textContent = elemento.autor;
         paginasAAgregar.textContent = elemento.paginas;
         estadoAAgregar.textContent = elemento.estado;
+        botonBorrar.textContent = "Borrar";
         libroAAgregar.appendChild(tituloAAgregar);
         libroAAgregar.appendChild(autorAAgregar);
         libroAAgregar.appendChild(paginasAAgregar);
         libroAAgregar.appendChild(estadoAAgregar);
+        libroAAgregar.appendChild(botonBorrar);
         libros.appendChild(libroAAgregar);   
     }
 )
+}
+
+function refrescarPantalla(params) {
+    
 }
 
 let enviarFormulario = document.getElementById("agregar-libros");
@@ -73,6 +75,16 @@ function valorCheckbox(element) {
     else
        element.value="false";
 }
+
+function borrarLibro() {
+    let quieroBorrar = document.querySelectorAll(".botonBorrado")
+    quieroBorrar.forEach(boton => {
+
+    }
+    )
+}
+
+
 
 /* clase añadir form-to-add */
  /*    enviarFormulario.onsubmit = event.preventDefault();
